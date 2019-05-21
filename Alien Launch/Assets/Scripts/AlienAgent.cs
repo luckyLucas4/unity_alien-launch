46d50a9a928addcc56e43fd3dadfafa44686f7ff
luckyLucas - Main <lucas.granberg00@gmail.com>
2019-05-21 20:41:04 +0200

    Initial commit

---
+++ Alien Launch/Assets/Scripts/AlienAgent.cs
@@ -0,0 +1,19 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
